f1b8f7388ce453d294e3a3b4792c0ebc189e2e4f
meugenia1 <meps@cin.ufpe.br>
2019-10-27 08:58:12 -0300

    most important features
    adicionando o código do random forest que lista a importância das features (sala e sexo inclusos)

--- pre-processing.py
+++ pre-processing.py
@@ -111,3 +111,19 @@ plt.xlabel('Número de componentes')
 plt.ylabel('Variância explicada cumulativa')
 plt.title('Análise da variância explicada com PCA\n(normalização MinMax)', size=16)
 plt.show()
+
+# *** Usando random forest para achar as features mais importantes ***
+
+# Split the data into 40% test and 60% training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+
+# Create a random forest classifier
+clf = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Lista com importância de cada feature
+for feature in zip(df.columns, clf.feature_importances_):
+    print(feature)
+    
